68e0eff9c902bc7a71bd8680add4a3e23d5f961f
Erin Turgut <erintur@meta.com>
2025-06-04 22:15:07 -0700

    added more styling and modal pop up is now fully functional

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,27 +1,33 @@
-const modal = document.querySelector(".modal-overlay");
-const cards = document.querySelectorAll(".card");
+const modalOverlay = document.querySelector(".modal-overlay");
+const modalPopup = document.querySelector(".modal-popup");
+let currCard = null;
+// const cards = document.querySelectorAll(".card");
 
 //click on a card, show modal with greyed out background
 
 function displayModal() {
-    modal.style.display = "block";
-    
+    currCard.style.borderStyle = "inset";
+    modalOverlay.style.display = "block";
+    modalPopup.style.display = "block";
 }
 
 function hideModal() {
-
+    currCard.style.borderStyle = "outset";
+    modalOverlay.style.display = "none";
+    modalPopup.style.display = "none";
 }
 
 window.addEventListener("click", function (event) {
     if (event.target.className === "card") {
+        currCard = event.target;
         displayModal();
-    }  else if (event.target.className === "card") {
-        
+    }  else if (event.target.className === "modal-overlay") {
+        hideModal();
     }
 });
 
-for(const card of cards) {
-    card.addEventListener("click", () => {
-       displayModal();
-    })
-};
+// for(const card of cards) {
+//     card.addEventListener("click", () => {
+//        displayModal();
+//     })
+// };
