559550bba464eaf51dc4d225f9c9dad1c1263039
Erin Turgut <erintur@meta.com>
2025-06-04 17:23:18 -0700

    progress on improved modal display

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,27 @@
+const modal = document.querySelector(".modal-overlay");
+const cards = document.querySelectorAll(".card");
 
+//click on a card, show modal with greyed out background
+
+function displayModal() {
+    modal.style.display = "block";
+    
+}
+
+function hideModal() {
+
+}
+
+window.addEventListener("click", function (event) {
+    if (event.target.className === "card") {
+        displayModal();
+    }  else if (event.target.className === "card") {
+        
+    }
+});
+
+for(const card of cards) {
+    card.addEventListener("click", () => {
+       displayModal();
+    })
+};
