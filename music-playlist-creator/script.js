d0d0c742741d937a490e13e693702080e69a3999
Erin Turgut <erintur@meta.com>
2025-06-05 17:06:35 -0700

    all data on main page and within modal is dynamically loaded

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,4 +1,3 @@
-// import playlistData from "./data/playlistData";
 
 document.addEventListener('DOMContentLoaded', () => {
     loadPlaylists();
@@ -7,7 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const modalOverlay = document.querySelector(".modal-overlay");
 const modalPopup = document.querySelector(".modal-popup");
-let currCard = null;
+let currPlaylistId = null;
+let currPlaylist = null;
 
 function loadPlaylists() {
     //load playlist objects from data.js
@@ -30,6 +30,7 @@ function loadPlaylists() {
 function createPlaylistCard(playlist) {
     const playlistCard = document.createElement('div');
     playlistCard.className = "card";
+    playlistCard.id = playlist.playlistID; 
     playlistCard.innerHTML = `
         <img src=${playlist.playlist_art} alt="image of ${playlist.playlist_author}'s ${playlist.playlist_name}"/>
         <h3>${playlist.playlist_name}</h3>
@@ -39,16 +40,83 @@ function createPlaylistCard(playlist) {
     return playlistCard; 
 }
 
+function obtainPlaylistInfo() {
+
+    currPlaylist = playlistData.find((playlist) => playlist.playlistID == currPlaylistId);
+    console.log(currPlaylist);
+}
 
 //want to make the below code cleaner and have more separation of concerns
+//make these toggles instead???
 function displayModal() {
-    currCard.style.borderStyle = "inset";
+    // currCard.style.borderStyle = "inset"; //NEED TO FIX
     modalOverlay.style.display = "block";
     modalPopup.style.display = "block";
+
+
+
+    const modalContent = document.querySelector(".modal-content");
+    // modalContent.innerHTML = `
+    //     <div class="playlist-info">
+    //         <img src=${currPlaylist.playlist_art} alt="image of ${currPlaylist.playlist_author}'s ${currPlaylist.playlist_name}"/> 
+    //         <section>
+    //             <h3>${currPlaylist.playlist_name}</h3>
+    //             <p>${currPlaylist.playlist_author}</p>
+    //         </section>
+    //     </div>
+    //     <div class="playlist-list">
+            // <div class="song-tile">
+            //     <img src="./assets/img/song.png" alt="image of music note"/>
+            //     <section>
+            //         <p class="song-title">Song Title</p>
+            //         <p>Artist Name</p>
+            //         <p>Album Name</p>
+            //     </section>
+            //     <p class="song-length">0:00</p>
+            // </div>
+    //     </div>
+    // `
+    modalContent.innerHTML = `
+        <div class="playlist-info">
+            <img src=${currPlaylist.playlist_art} alt="image of ${currPlaylist.playlist_author}'s ${currPlaylist.playlist_name}"/> 
+            <section>
+                <h3>${currPlaylist.playlist_name}</h3>
+                <p>${currPlaylist.playlist_author}</p>
+            </section>
+        </div>
+        <div class="playlist-list">
+        </div>
+    `
+
+    currPlaylist.songs.forEach( (song) => {
+            const songTile = createSongTile(song);
+            songTilesArea = document.querySelector(".playlist-list");
+            songTilesArea.appendChild(songTile);
+    });
+
+}
+
+function createSongTile(song) {
+    console.log(song);
+    const newSongTile = document.createElement('div');
+    newSongTile.className = "song-tile";
+    // playlistCard.id = playlist.playlistID; 
+    newSongTile.innerHTML = `
+        <div class="song-tile">
+            <img src=${song.cover_art} alt="image of ${song.title}"/>
+            <section>
+                <p class="song-title">${song.title}</p>
+                <p>${song.artist}</p>
+                <p>${song.album}</p>
+            </section>
+            <p class="song-length">${song.length}</p>
+        </div>
+    `
+    return newSongTile; 
 }
 
 function hideModal() {
-    currCard.style.borderStyle = "outset";
+    // currCard.style.borderStyle = "outset";
     modalOverlay.style.display = "none";
     modalPopup.style.display = "none";
 }
@@ -57,7 +125,9 @@ function hideModal() {
 
 window.addEventListener("click", function (event) {
     if (event.target.className === "card") {
-        currCard = event.target;
+        currPlaylistId = event.target.id;
+        console.log(currPlaylistId);
+        obtainPlaylistInfo();
         displayModal();
     }  else if (event.target.className === "modal-overlay") {
         hideModal();
